c37ffc4d38116b551c63d9df2a0b96e958b9491d
Al <mawaskow@yahoo.com>
2023-07-20 16:34:55 +0100

    adds user input form

--- modules.py
+++ modules.py
@@ -32,55 +32,90 @@ class Configuration(object):
             result = Configuration.__load__(json.loads(f.read()))
         return result
 
-class ConfigForm(FlaskForm):
-    cred_p_hect_p_yr = FloatField(label=("Credits per Hectare per Year: "), validators=[DataRequired(), NumberRange(min=0, max=9999)])
-    nom_interest_rt = FloatField(label= ('Nominal Interest Rate: '), validators=[DataRequired(), NumberRange(min=0.001, max=0.999)])
-    inflation_rt = FloatField(label= ('Inflation Rate: '), validators=[DataRequired(), NumberRange(min=0.001, max=0.999)])
-    reg_acct_opening_fee = FloatField(label= ('Registry Account Opening Fee: '), validators=[DataRequired(), NumberRange(min=0, max=9999)])
-    reg_lsting_cost_p_credit = FloatField(label= ('Registry Listing Cost per Credit: '), validators=[DataRequired(), NumberRange(min=0, max=9999)])
-    reg_conv_cost_fee_p_inspect = FloatField(label= ('Registry Conversion Cost Fee per Inspection: '), validators=[DataRequired(), NumberRange(min=0, max=9999)])
-    reg_conv_cost_p_credit_abv_min_thresh_of_credit = FloatField(label= ('Registry Conversion Cost per Credit above Minimum Threshold of Credits: '), validators=[DataRequired(), NumberRange(min=0.001, max=0.999)])
-    reg_levy_cost_p_credit = FloatField(label= ('Registry Levy Cost per Credit: '), validators=[DataRequired(), NumberRange(min=0.001, max=0.999)])
-    valid_and_verif_app_cost_p_inspect = FloatField(label= ('Validation and Verification Application Cost per Inspection: '), validators=[DataRequired(), NumberRange(min=0, max=9999)])
-    valid_and_verif_stmt_cost_p_inspect = FloatField(label= ('Validation and Verification Statement Cost per Inspection: '), validators=[DataRequired(), NumberRange(min=0, max=9999)])
-    valid_and_verif_inspctr_travel_costs_p_inspect = FloatField(label= ('Validation and Verification Inspector Travel Costs per Inspection: '), validators=[DataRequired(), NumberRange(min=0, max=9999)])
-    inspect_cycle_length = FloatField(label= ('Inspection Cycle Length: '), validators=[DataRequired(), NumberRange(min=0, max=9999)])
-    min_thresh_of_credits = FloatField(label= ('Minimum Threshold of Credits: '), validators=[DataRequired(), NumberRange(min=0, max=9999)])
-    interest_rate = FloatField(label= ('Interest Rate: '), validators=[DataRequired(), NumberRange(min=0.001, max=0.999)])
-    payments_p_yr = IntegerField(label= ('Payments per Year: '), validators=[DataRequired(), NumberRange(min=0, max=9999)])
+
+####################
+###   FFP TOOL   ###
+####################
+
+class FFPAssumForm(FlaskForm):
+    '''
+    Creates form for modifying the FFP Tool's assumptions
+    '''
+    cred_p_hect_p_yr = FloatField(label=("Credits per Hectare per Year: "), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    nom_interest_rt = FloatField(label= ('Nominal Interest Rate: '), validators=[DataRequired(), NumberRange(min=0.000000001, max=0.999999)])
+    inflation_rt = FloatField(label= ('Inflation Rate: '), validators=[DataRequired(), NumberRange(min=0.000000001, max=0.999999)])
+    reg_acct_opening_fee = FloatField(label= ('Registry Account Opening Fee: '), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    reg_lsting_cost_p_credit = FloatField(label= ('Registry Listing Cost per Credit: '), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    reg_conv_cost_fee_p_inspect = FloatField(label= ('Registry Conversion Cost Fee per Inspection: '), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    reg_conv_cost_p_credit_abv_min_thresh_of_credit = FloatField(label= ('Registry Conversion Cost per Credit above Minimum Threshold of Credits: '), validators=[DataRequired(), NumberRange(min=0.000000001, max=0.999999)])
+    reg_levy_cost_p_credit = FloatField(label= ('Registry Levy Cost per Credit: '), validators=[DataRequired(), NumberRange(min=0.000000001, max=0.999999)])
+    valid_and_verif_app_cost_p_inspect = FloatField(label= ('Validation and Verification Application Cost per Inspection: '), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    valid_and_verif_stmt_cost_p_inspect = FloatField(label= ('Validation and Verification Statement Cost per Inspection: '), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    valid_and_verif_inspctr_travel_costs_p_inspect = FloatField(label= ('Validation and Verification Inspector Travel Costs per Inspection: '), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    inspect_cycle_length = FloatField(label= ('Inspection Cycle Length: '), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    min_thresh_of_credits = FloatField(label= ('Minimum Threshold of Credits: '), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    interest_rate = FloatField(label= ('Interest Rate: '), validators=[DataRequired(), NumberRange(min=0.000000001, max=0.999999)])
+    payments_p_yr = IntegerField(label= ('Payments per Year: '), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
     submit=SubmitField("Submit")
+
+class FFPUserInputForm(FlaskForm):
+    '''
+    Creates form for modifying the FFP Tool's user input
+    '''
+    num_yrs = FloatField(label=("Period of Years: "), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    cred_p_hect_p_yr = FloatField(label=("Credits per Hectare per Year: "), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    hect_restored = FloatField(label=("Hectares Restored: "), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    invest_amt = FloatField(label=("Investment Amount: "), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    start_yr = IntegerField(label= ('Start Year: '), validators=[DataRequired(), NumberRange(min=1900, max=3000)])
+    price_p_cred = FloatField(label=("Price per Credit: "), validators=[DataRequired(), NumberRange(min=0, max=9999999)])
+    submit=SubmitField("Submit")
+
+####################
+###   SET TOOL   ###
+####################
+
 
 '''
 Function Definitions
 '''
 
-def parse_config(CONFIG_FILE):
-    # reads the json file into a list of values
-    config_json = Configuration.load_json(CONFIG_FILE)
-    cred_p_hect_p_yr = config_json.avg_cred_p_hect_p_yr
-    nom_interest_rt = config_json.nom_int_rt
-    inflation_rt = config_json.inflation_rt
-    reg_acct_opening_fee = config_json.reg_acct_open_fee
-    reg_lsting_cost_p_credit = config_json.reg_listing_cost_p_credit
-    reg_conv_cost_fee_p_inspect = config_json.reg_conv_cost_fee_p_inspect
-    reg_conv_cost_p_credit_abv_min_thresh_of_credit = config_json.reg_conv_cost_p_cred_abv_min_thresh_of_credits
-    reg_levy_cost_p_credit = config_json.reg_levy_cost_p_cred
-    valid_and_verif_app_cost_p_inspect = config_json.valid_and_verif_app_cost_p_inspect
-    valid_and_verif_stmt_cost_p_inspect = config_json.valid_and_verif_stmnt_cost_p_inspect
-    valid_and_verif_inspctr_travel_costs_p_inspect = config_json.valid_and_verif_inspectr_travel_cost_p_inspect
-    inspect_cycle_length = config_json.inspect_cycle_len
-    min_thresh_of_credits = config_json.min_thresh_of_credits
-    interest_rate = config_json.interest_rt
-    payments_p_yr = config_json.payments_p_yr
+####################
+###   FFP TOOL   ###
+####################
+
+def parse_assum(ASSUM_FILE):
+    '''
+    Parses the FFP Tool's assumption json file into a list of values
+    '''
+    assum_json = Configuration.load_json(ASSUM_FILE)
+    cred_p_hect_p_yr = assum_json.avg_cred_p_hect_p_yr
+    nom_interest_rt = assum_json.nom_int_rt
+    inflation_rt = assum_json.inflation_rt
+    reg_acct_opening_fee = assum_json.reg_acct_open_fee
+    reg_lsting_cost_p_credit = assum_json.reg_listing_cost_p_credit
+    reg_conv_cost_fee_p_inspect = assum_json.reg_conv_cost_fee_p_inspect
+    reg_conv_cost_p_credit_abv_min_thresh_of_credit = assum_json.reg_conv_cost_p_cred_abv_min_thresh_of_credits
+    reg_levy_cost_p_credit = assum_json.reg_levy_cost_p_cred
+    valid_and_verif_app_cost_p_inspect = assum_json.valid_and_verif_app_cost_p_inspect
+    valid_and_verif_stmt_cost_p_inspect = assum_json.valid_and_verif_stmnt_cost_p_inspect
+    valid_and_verif_inspctr_travel_costs_p_inspect = assum_json.valid_and_verif_inspectr_travel_cost_p_inspect
+    inspect_cycle_length = assum_json.inspect_cycle_len
+    min_thresh_of_credits = assum_json.min_thresh_of_credits
+    interest_rate = assum_json.interest_rt
+    payments_p_yr = assum_json.payments_p_yr
     # pack values into list
     jsondata = [cred_p_hect_p_yr, nom_interest_rt, inflation_rt, reg_acct_opening_fee, reg_lsting_cost_p_credit, reg_conv_cost_fee_p_inspect, reg_conv_cost_p_credit_abv_min_thresh_of_credit, reg_levy_cost_p_credit, valid_and_verif_app_cost_p_inspect, valid_and_verif_stmt_cost_p_inspect, valid_and_verif_inspctr_travel_costs_p_inspect, inspect_cycle_length, min_thresh_of_credits, interest_rate, payments_p_yr]
     return jsondata
 
-def populate_config(CONFIG_FILE, formdata):
+def update_assum(ASSUM_FILE, formdata):
+    '''
+    Updates the FFP Tool assumption json with a list of values from a form
+    '''
     # unpack values from list
     [cred_p_hect_p_yr, nom_interest_rt, inflation_rt, reg_acct_opening_fee, reg_lsting_cost_p_credit, reg_conv_cost_fee_p_inspect, reg_conv_cost_p_credit_abv_min_thresh_of_credit, reg_levy_cost_p_credit, valid_and_verif_app_cost_p_inspect, valid_and_verif_stmt_cost_p_inspect, valid_and_verif_inspctr_travel_costs_p_inspect, inspect_cycle_length, min_thresh_of_credits, interest_rate, payments_p_yr] = formdata
-    with open(CONFIG_FILE, "r") as config_json:
-        data = json.load(config_json)
+    # load the data from the current assumption file [for structure]
+    with open(ASSUM_FILE, "r") as assum_json:
+        data = json.load(assum_json)
     data["avg_cred_p_hect_p_yr"] = cred_p_hect_p_yr
     data["nom_int_rt"] = nom_interest_rt
     data["inflation_rt"] = inflation_rt
@@ -96,6 +131,44 @@ def populate_config(CONFIG_FILE, formdata):
     data["min_thresh_of_credits"] = min_thresh_of_credits
     data["interest_rt"] = interest_rate
     data["payments_p_yr"] = payments_p_yr
-    
-    with open(CONFIG_FILE, "w") as config_json:
-        json.dump(data, config_json)
+    # overwrite the file with new data values
+    with open(ASSUM_FILE, "w") as assum_json:
+        json.dump(data, assum_json)
+
+def parse_usrinp(USRINP_FILE):
+    '''
+    Parses the FFP Tool's user input json file into a list of values
+    '''
+    usrinp_json = Configuration.load_json(USRINP_FILE)
+    num_yrs = usrinp_json.num_yrs
+    cred_p_hect_p_yr = usrinp_json.cred_p_hect_p_yr
+    hect_restored = usrinp_json.hect_restored
+    invest_amt = usrinp_json.invest_amt
+    start_yr = usrinp_json.start_yr
+    price_p_cred = usrinp_json.price_p_cred
+    jsondata = [num_yrs, cred_p_hect_p_yr, hect_restored, invest_amt, start_yr, price_p_cred]
+    return jsondata
+
+def update_usrinp(USRINP_FILE, formdata):
+    '''
+    Updates the FFP Tool user input json with a list of values from a form
+    '''
+    # unpack values from list
+    [num_yrs, cred_p_hect_p_yr, hect_restored, invest_amt, start_yr, price_p_cred] = formdata
+    # load the data from the current user input file [for structure]
+    with open(USRINP_FILE, "r") as usrinp_json:
+        data = json.load(usrinp_json)
+    data["num_yrs"] = num_yrs
+    data["cred_p_hect_p_yr"] = cred_p_hect_p_yr
+    data["hect_restored"] = hect_restored
+    data["invest_amt"] = invest_amt
+    data["start_yr"] = start_yr
+    data["price_p_cred"] = price_p_cred
+    # overwrite the file with new data values
+    with open(USRINP_FILE, "w") as usrinp_json:
+        json.dump(data, usrinp_json)
+
+####################
+###   SET TOOL   ###
+####################
+
--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from flask import url_for, render_template, flash, request, redirect, send_from_
 from werkzeug.utils import secure_filename
 import glob
 #
-from modules import parse_config, populate_config, ConfigForm
+from modules import parse_assum, update_assum, FFPAssumForm, FFPUserInputForm, parse_usrinp, update_usrinp
 # temp until analysis can be in modules
 
 # powershell: $env:FLASK_APP = "run"
@@ -22,15 +22,21 @@ ALLOWED_EXTENSIONS = {'txt', 'json'}
 SECRET_KEY = "please_dont_hack_us_thanks"
 app.config['SECRET_KEY'] = SECRET_KEY
 
-INIT_CONFIG_FILE = "./static/initial_assumptions.json"
-FIN_CONFIG_FILE = "./inputs/final_assumptions.json"
+#FFP ASSUMPTION FORM
+FFP_INIT_ASSUM_FILE = "./static/initial_assumptions.json"
+FFP_FIN_ASSUM_FILE = "./inputs/final_assumptions.json"
+#FFP USER INPUT FORM
+FFP_INIT_USR_INP_FILE = "./static/user_input_default.json"
+FFP_FIN_USR_INP_FILE = "./inputs/user_input_data.json"
 
 '''
 Internal Functions
 '''
-# initialize final config file with default values
-jsondata = parse_config(INIT_CONFIG_FILE)
-populate_config(FIN_CONFIG_FILE, jsondata)
+#FFP TOOL
+# initialize final assumption file with default values
+jsondata = parse_assum(FFP_INIT_ASSUM_FILE)
+update_assum(FFP_FIN_ASSUM_FILE, jsondata)
+# initialize final user input file with default values
 
 '''
 ROUTES
@@ -45,14 +51,23 @@ def homepage():
 
 @app.route('/ffptool', methods=['GET', 'POST'])
 def ffp_tool():
-    form = ConfigForm()
-    if form.validate_on_submit():
-        formdata = [form.cred_p_hect_p_yr.data, form.nom_interest_rt.data, form.inflation_rt.data, form.reg_acct_opening_fee.data, form.reg_lsting_cost_p_credit.data, form.reg_conv_cost_fee_p_inspect.data, form.reg_conv_cost_p_credit_abv_min_thresh_of_credit.data, form.reg_levy_cost_p_credit.data, form.valid_and_verif_app_cost_p_inspect.data, form.valid_and_verif_stmt_cost_p_inspect.data, form.valid_and_verif_inspctr_travel_costs_p_inspect.data, form.inspect_cycle_length.data, form.min_thresh_of_credits.data, form.interest_rate.data, form.payments_p_yr.data]
-        populate_config(FIN_CONFIG_FILE, formdata)
-        return redirect(url_for('results'))
-    formdata = parse_config(FIN_CONFIG_FILE)
-    [form.cred_p_hect_p_yr.data, form.nom_interest_rt.data, form.inflation_rt.data, form.reg_acct_opening_fee.data, form.reg_lsting_cost_p_credit.data, form.reg_conv_cost_fee_p_inspect.data, form.reg_conv_cost_p_credit_abv_min_thresh_of_credit.data, form.reg_levy_cost_p_credit.data, form.valid_and_verif_app_cost_p_inspect.data, form.valid_and_verif_stmt_cost_p_inspect.data, form.valid_and_verif_inspctr_travel_costs_p_inspect.data, form.inspect_cycle_length.data, form.min_thresh_of_credits.data, form.interest_rate.data, form.payments_p_yr.data] = formdata
-    return render_template("ffp_tool.html", form=form)
+    asubmitted=False
+    usubmitted= False
+    aform = FFPAssumForm()
+    uform = FFPUserInputForm()
+    if aform.validate_on_submit():
+        aformdata = [aform.cred_p_hect_p_yr.data, aform.nom_interest_rt.data, aform.inflation_rt.data, aform.reg_acct_opening_fee.data, aform.reg_lsting_cost_p_credit.data, aform.reg_conv_cost_fee_p_inspect.data, aform.reg_conv_cost_p_credit_abv_min_thresh_of_credit.data, aform.reg_levy_cost_p_credit.data, aform.valid_and_verif_app_cost_p_inspect.data, aform.valid_and_verif_stmt_cost_p_inspect.data, aform.valid_and_verif_inspctr_travel_costs_p_inspect.data, aform.inspect_cycle_length.data, aform.min_thresh_of_credits.data, aform.interest_rate.data, aform.payments_p_yr.data]
+        update_assum(FFP_FIN_ASSUM_FILE, aformdata)
+        asubmitted=True
+    if uform.validate_on_submit():
+        uformdata = [uform.num_yrs.data, uform.cred_p_hect_p_yr.data, uform.hect_restored.data, uform.invest_amt.data, uform.start_yr.data, uform.price_p_cred.data]
+        update_usrinp(FFP_FIN_USR_INP_FILE, uformdata)
+        usubmitted=True
+    aformdata = parse_assum(FFP_FIN_ASSUM_FILE)
+    [aform.cred_p_hect_p_yr.data, aform.nom_interest_rt.data, aform.inflation_rt.data, aform.reg_acct_opening_fee.data, aform.reg_lsting_cost_p_credit.data, aform.reg_conv_cost_fee_p_inspect.data, aform.reg_conv_cost_p_credit_abv_min_thresh_of_credit.data, aform.reg_levy_cost_p_credit.data, aform.valid_and_verif_app_cost_p_inspect.data, aform.valid_and_verif_stmt_cost_p_inspect.data, aform.valid_and_verif_inspctr_travel_costs_p_inspect.data, aform.inspect_cycle_length.data, aform.min_thresh_of_credits.data, aform.interest_rate.data, aform.payments_p_yr.data] = aformdata
+    uformdata = parse_usrinp(FFP_FIN_USR_INP_FILE)
+    [uform.num_yrs.data, uform.cred_p_hect_p_yr.data, uform.hect_restored.data, uform.invest_amt.data, uform.start_yr.data, uform.price_p_cred.data] = uformdata
+    return render_template("ffp_tool.html", aform=aform, asubmitted=asubmitted, uform=uform, usubmitted=usubmitted)
 
 @app.route('/settool', methods=['GET', 'POST'])
 def set_tool():
