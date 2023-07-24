6f96d9f8770141c724aef7bac6d7fda175396fff
Al <mawaskow@yahoo.com>
2023-07-24 15:47:35 +0100

    adds formatted results below form on same page

--- run.py
+++ run.py
@@ -54,23 +54,41 @@ def homepage():
 
 @app.route('/ffptool', methods=['GET', 'POST'])
 def ffp_tool():
+    #initialize submission notification variables
     asubmitted=False
     usubmitted= False
+    #initialize forms from modules.py -defined classes
     aform = FFPAssumForm()
     uform = FFPUserInputForm()
+    # if assumption form is submitted
     if aform.validate_on_submit():
+        # store the information from the form into a list
         aformdata = [aform.avg_cred_p_hect_p_yr.data, aform.nom_int_rt.data, aform.inflation_rt.data, aform.reg_acct_open_fee.data, aform.reg_listing_cost_p_credit.data, aform.reg_conv_cost_fee_p_inspect.data, aform.reg_conv_cost_p_cred_abv_min_thresh_of_credits.data, aform.reg_levy_cost_p_cred.data, aform.valid_and_verif_app_cost_p_inspect.data, aform.valid_and_verif_stmnt_cost_p_inspect.data, aform.valid_and_verif_inspctr_travel_cost_p_inspect.data, aform.inspect_cycle_len.data, aform.min_thresh_of_credits.data, aform.interest_rt.data, aform.payments_p_yr.data]
+        # and update the assumption json file with the information
         update_assum(FFP_FIN_ASSUM_FILE, aformdata)
         asubmitted=True
+    # if the user input form is submitted
     if uform.validate_on_submit():
+        # store the information from the form into a list
         uformdata = [uform.num_yrs.data, uform.cred_p_hect_p_yr.data, uform.hect_restored.data, uform.invest_amt.data, uform.start_yr.data, uform.price_p_cred.data]
+        # and update the user input json file with the information
         update_usrinp(FFP_FIN_USR_INP_FILE, uformdata)
         usubmitted=True
+    # read the assumption information from the json (to display default values and updated values)
     aformdata = parse_assum(FFP_FIN_ASSUM_FILE)
+    # unpack the information so it can be sent to the html
     [aform.avg_cred_p_hect_p_yr.data, aform.nom_int_rt.data, aform.inflation_rt.data, aform.reg_acct_open_fee.data, aform.reg_listing_cost_p_credit.data, aform.reg_conv_cost_fee_p_inspect.data, aform.reg_conv_cost_p_cred_abv_min_thresh_of_credits.data, aform.reg_levy_cost_p_cred.data, aform.valid_and_verif_app_cost_p_inspect.data, aform.valid_and_verif_stmnt_cost_p_inspect.data, aform.valid_and_verif_inspctr_travel_cost_p_inspect.data, aform.inspect_cycle_len.data, aform.min_thresh_of_credits.data, aform.interest_rt.data, aform.payments_p_yr.data] = aformdata
+    # read the user input information from the json (to display default values and updated values)
     uformdata = parse_usrinp(FFP_FIN_USR_INP_FILE)
+    # unpack the information so it can be sent to the html
     [uform.num_yrs.data, uform.cred_p_hect_p_yr.data, uform.hect_restored.data, uform.invest_amt.data, uform.start_yr.data, uform.price_p_cred.data] = uformdata
-    return render_template("ffp_tool.html", aform=aform, asubmitted=asubmitted, uform=uform, usubmitted=usubmitted)
+    # displays results beneath forms
+    # calculates the result of the input files
+    Output_From_Json(FFP_FIN_USR_INP_FILE, FFP_FIN_ASSUM_FILE)
+    FFP_SIMPLE_RESULT = "./outputs/simple_output.json"
+    with open(FFP_SIMPLE_RESULT, "r") as result_json:
+        result_json_text = json.load(result_json)
+    return render_template("ffp_tool.html", aform=aform, asubmitted=asubmitted, uform=uform, usubmitted=usubmitted, result_json_text = result_json_text)
 
 @app.route('/settool', methods=['GET', 'POST'])
 def set_tool():
