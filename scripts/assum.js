66dd20ec783fd07028e46965c229863ef9e3ccba
Al <mawaskow@yahoo.com>
2023-07-26 14:17:20 +0100

    updates css styling with bootstrap, adds first local scripts
    (now the new boolean fields arent't working but we'll figure that out later)

---
+++ scripts/assum.js
@@ -0,0 +1,14 @@
+$('#update-assum').on('click', function(event){
+    var form = $('assum-form');
+    var form_id = 'assum-form';
+    var url = form.prop('action');
+    var type = form.prop('method');
+    var formData = getAssumFormData(form_id);
+
+    send_form(form, form_id, url, type, modular_ajax, formData);
+});
+
+function getAssumFormData(form_id) {
+    var formData = new FormData(document.getElementById(form_id));
+    return formData
+}
