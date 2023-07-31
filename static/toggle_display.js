5694a4baa2ccc72e35a418d551acb27dbe9093c8
Al <mawaskow@yahoo.com>
2023-07-31 12:59:28 +0100

    trying to fix assumptions glitch

--- static/toggle_display.js
+++ static/toggle_display.js
@@ -1,9 +1,26 @@
+
 function toggle_display(){
+    console.log("ffs");
+    el = document.selectElementById("assum-form-div");
+    
+    if(el.style.display == 'none'){
+        el.style.display = 'block'
+    }else if(el.style.display == 'block'){
+        el.style.display = 'none'
+    }else{
+        el.style.display = 'none'
+    }
+};
+
+/*
+$('#assum_disp_btn').on('click', function(event){
     el = document.selectElementByID("assum-form-div");
     
     if(el.style.display == 'none'){
         el.style.display = 'block'
+    }else if(el.style.display == 'block'){
+        el.style.display = 'none'
     }else{
-       el.style.display = 'none'
+        el.style.display = 'none'
     }
-  }
+});*/
