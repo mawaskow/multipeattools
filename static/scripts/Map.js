47b824920eeedee3102691ca5e9042324da4f43b
Al <mawaskow@yahoo.com>
2023-10-18 16:09:56 +0100

    Expands map to encompass all europe

--- static/scripts/Map.js
+++ static/scripts/Map.js
@@ -68,11 +68,10 @@ const osmLayer=new TileLayer({
 });
 
 const view=new View({
-    extent:[-1189593, 6692152.5, -665102.8125, 7450535],
-    // extent of boglayer//extent:[-882658.5, 6978113.4, -749958.0, 7124488.3],
-    //center:[0,0],
-    center:[816308.25,7051300.85],
-    zoom:0,
+    //extent:[-1189593, 6692152.5, -665102.8125, 7450535], // ireland
+    extent:[-2005155, 3723095, 3711745, 8600839], // europe
+    center:[-816308.25,7051300.85],
+    zoom:7.5,
     projection: mapProjection
 });
 
